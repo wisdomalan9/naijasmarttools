ba9936563811a0111fdd8050008c9a6a89917888
wisdom Alan okonedo <wisdomalan9@gmail.com>
2026-03-09 23:38:34 +0100

    Fix service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,13 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker Installed');
+const CACHE_NAME = "naijasmarttools-v1";
+
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker Activated");
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
