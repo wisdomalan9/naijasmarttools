d88d814412de5456e5fef1de085a972790a8e1af
wisdom Alan okonedo <wisdomalan9@gmail.com>
2026-03-09 23:47:54 +0100

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,15 @@
 const CACHE_NAME = "naijasmarttools-v1";
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   console.log("Service Worker Installed");
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   console.log("Service Worker Activated");
+  return self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
